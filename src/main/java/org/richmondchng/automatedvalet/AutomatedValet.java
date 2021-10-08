96442a00d8c2d29a27bad46d507a229a455c876d
richmondchng <richmondchng@gmail.com>
2021-10-08 13:00:54 +0800

    add main class

---
+++ src/main/java/org/richmondchng/automatedvalet/AutomatedValet.java
@@ -0,0 +1,12 @@
+package org.richmondchng.automatedvalet;
+
+/**
+ * Main class.
+ * @author richmondchng
+ */
+public class AutomatedValet {
+
+    public static void main(final String[] args) {
+
+    }
+}
